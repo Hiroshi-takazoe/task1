f98997c84b49b1f9b98f0a50678c48131664a5ee
高添　浩司 <takazoe@MacBook-Pro.local>
2020-07-15 20:04:33 +0900

    modified2

--- challenge1/script.js
+++ challenge1/script.js
@@ -5,28 +5,28 @@ function fizzBuzz() {
   while (output.firstChild) {
     output.removeChild(output.firstChild);
   }
-  var fizz = Number(document.getElementById("fizz").value);
-  var buzz = Number(document.getElementById("buzz").value);
-  var num1 = regex.test(fizz);
-  var num2 = regex.test(buzz);
-  if (num1 && num2) {
+  let fizzNumber = Number(document.getElementById("fizz").value);
+  let buzzNumber = Number(document.getElementById("buzz").value);
+  let isFizzNumber = regex.test(fizzNumber);
+  let isBuzzNumber = regex.test(buzzNumber);
+  if (isFizzNumber && isBuzzNumber) {
     for (i = 1; i <= 100; i++) {
-      if (i % fizz == 0 && i % buzz == 0) {
-        var p = document.createElement("p");
+      if (i % fizzNumber == 0 && i % buzzNumber == 0) {
+        let p = document.createElement("p");
         p.innerHTML = "FizzBuzz " + i;
         output.appendChild(p);
-      } else if (i % fizz == 0) {
-        var p = document.createElement("p");
+      } else if (i % fizzNumber == 0) {
+        let p = document.createElement("p");
         p.innerHTML = "Fizz " + i;
         output.appendChild(p);
-      } else if (i % buzz == 0) {
-        var p = document.createElement("p");
+      } else if (i % buzzNumber == 0) {
+        let p = document.createElement("p");
         p.innerHTML = "Buzz " + i;
         output.appendChild(p);
       };
     };
   } else {
-    var p = document.createElement("p");
+    let p = document.createElement("p");
     p.innerHTML = "整数値を入力してください。";
     output.appendChild(p);
   };
